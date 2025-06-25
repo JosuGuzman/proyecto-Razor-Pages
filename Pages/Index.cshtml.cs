81a54c91eb375d38d0c9597803738160f6083b35
JosuGuzman <Josu.guzmanet12d1@gmail.com>
2025-06-25 16:43:54 -0300

    Primer commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PrimerRazorPages.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    
+    public string? NombreUsuario {get; set;}
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+    public void OnPost(string Nombre)
+    {
+        NombreUsuario = nombre;
+    }
+}
